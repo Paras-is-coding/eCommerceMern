86c807588e2e17fc0996012e38af9967b2987817
Paras-is-coding <paraschand5815@gmail.com>
2024-04-03 17:18:50 +0545

    Removed previous changes made for deployment

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const server = http.createServer(app);
 
 
 // listening to server in any port on server
-server.listen(process.env.PORT || 80,'localhost',(err)=>{
+server.listen(process.env.PORT,'localhost',(err)=>{
     if(!err){
-        console.log("Server is running in port 443/80")
+        console.log("Server is running in port 3000")
         console.log("Press CTRL+C to disconnect your server")
         console.log("Use http://localhost:3000/ to browse your server")
     }
